3939fff659725076b85eb59b70e67fad5ebb2713
YOusuf-Abdullah-Borna <y26born@siena.edu>
2019-12-08 12:15:00 -0500

    renders a color with white background on the screen

--- game_dev/golang/main.go
+++ game_dev/golang/main.go
@@ -6,19 +6,49 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	screenHeight = 600
+	screenWidth= 600
+)
+
 func main() {
 
-	window, err := sdl.CreateWindow("Testing sdl2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-
-	if err != nil {
-
-		fmt.Println(err)
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil{
+		fmt.Println("Initializing SDL:",err)
 		return
 	}
 
-	defer window.Destroy()
+	//CREATES THE WINDOW FOR THE GAME
+	window, err := sdl.CreateWindow(
+		"SDL all set",
+		sdl.WINDOWPOS_UNDEFINED,sdl.WINDOWPOS_UNDEFINED,screenWidth,screenHeight,
+		sdl.WINDOW_OPENGL)
 
-	sdl.Delay(14000)
+		if err != nil {
+			fmt.Println("Initializing window:",err)
+			return
+		}
+
+		defer window.Destroy()
+
+		//HELPS US DRAWING THINGS ON THE WINDOW
+	renderer,err := sdl.CreateRenderer(window,-1,sdl.RENDERER_ACCELERATED)
+
+	if err != nil {
+		fmt.Println("Initializing renderer:",err)
+		return
+	}
+
+	defer renderer.Destroy()
+
+
+	for{
+		//CREATES A COLOR OF WHITE
+		renderer.SetDrawColor(255,255,255,255)
+		renderer.Clear()
+		//HELPS US TO SHOW THE COLOR
+		renderer,Present()
+	}
+
 
 }
