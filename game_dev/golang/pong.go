923728c9c9bfa996bfae0f784617aa410dc6a1d0
YOusuf-Abdullah-Borna <y26born@siena.edu>
2019-12-09 00:48:57 -0500

    completed

--- game_dev/golang/pong.go
+++ game_dev/golang/pong.go
@@ -10,6 +10,14 @@ import (
 
 const winWidth, winHeight int = 800, 600
 
+type gameState int
+const (
+	start gameState = iota
+	play 
+)
+
+var state = start
+
 var nums = [][] byte{
 
 	{1,1,1,
@@ -83,6 +91,11 @@ type ball struct{
    color color
 }
 
+func lerp( a float32, b float32,pct float32) float32{
+
+	return a+ pct*(b-a)
+}
+
 func (ball *ball) draw(pixels []byte){
 
 	for y:= -ball.radius; y < ball.radius; y++{
@@ -116,25 +129,31 @@ func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle, elapsedTime fl
 	} 
 
 
-	if ball.x < 0 || int(ball.x) > winWidth{
+	if ball.x <0{
 
-		ball.pos =getCenter()
+		rightPaddle.score++
+		ball.pos = getCenter()
+		state =start
+	} else if int(ball.x) > winWidth{
+		 
+		 leftPaddle.score++
+		 ball.pos = getCenter()
+         state=start
+	}
 
-
-
-	} 
-
-	if ball.x < leftPaddle.x+leftPaddle.w/2 {
+	if ball.x - ball.radius < leftPaddle.x+leftPaddle.w/2 {
 
 		if ball.y > leftPaddle.y - leftPaddle.h/2 && ball.y < leftPaddle.y+leftPaddle.h/2{
 			ball.xv = -ball.xv
+			
 		}
 	}
 
-	if  ball.x > rightPaddle.x-rightPaddle.w/2 {
+	if  ball.x + ball.radius > rightPaddle.x-rightPaddle.w/2 {
 
 		if ball.y > rightPaddle.y - rightPaddle.h/2 && ball.y < rightPaddle.y+rightPaddle.h/2{
 			ball.xv = -ball.xv
+			
 		}
 	}
 
@@ -148,6 +167,7 @@ type paddle struct{
 	w float32
 	h float32
 	speed float32
+	score int 
 	color color
 }
 
@@ -164,6 +184,9 @@ func (paddle *paddle) draw( pixels []byte){
 			setPixel(startX+x,startY+y,paddle.color,pixels)
 		}
 	}
+
+	numX := lerp(paddle.x,getCenter().x,0.2)
+	drawNumber(pos{numX, 35},paddle.color,10,paddle.score,pixels)
 }
 
 
@@ -243,8 +266,8 @@ func main() {
 	
 
 	
-	player1 := paddle{pos{100,100},20,100,300,color{255,255,255}}
-	player2 := paddle{pos{float32(winWidth)-50 ,100},20,100,300,color{255,255,255}}
+	player1 := paddle{pos{100,100},20,100,300,0,color{255,255,255}}
+	player2 := paddle{pos{float32(winWidth)-50 ,100},20,100,300,0,color{255,255,255}}
 	ball := ball{pos{300,300},20,400,400,color{255,255,255}}
 	
 	keyState := sdl.GetKeyboardState()
@@ -260,14 +283,25 @@ func main() {
 				return
 			}
 		}
-	 clear(pixels)
 	 
-	 drawNumber(getCenter(),color{255,255,255},20,2,pixels)
-		
-
+	 if state == play{
+	 	drawNumber(getCenter(),color{255,255,255},20,2,pixels)
 		player1.update(keyState,elapsedTime)
 		player2.aiUpdate(&ball,elapsedTime)
-        ball.update( &player1,&player2,elapsedTime)
+		ball.update( &player1,&player2,elapsedTime)
+	 }else if state == start{
+
+		  if keyState[sdl.SCANCODE_SPACE] !=0{
+			   
+			if player1.score == 3 || player2.score == 3{
+			   player1.score = 0
+			   player2.score =0
+			}
+			   state = play
+			
+		  }
+	 }
+		clear(pixels)
 		ball.draw(pixels)
 		player1.draw(pixels)
 		player2.draw(pixels)
